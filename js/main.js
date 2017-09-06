c3573ebb065e591fff0d243b4471996cf8b0c0c7
zeku87 <nocomment182@gmail.com>
2017-09-07 01:29:10 +0200

    social intents added

--- js/main.js
+++ js/main.js
@@ -3,13 +3,21 @@ $(document).ready(function(){
 	$.getJSON(url, function(jsonFile){
 		$('#sentence').html(jsonFile['quote']);
 		$('#author').html(jsonFile['author']);
+		$('#twitter-intent').attr('href','https://twitter.com/intent/tweet?text=' + jsonFile['quote']
+		 	 + ' -' + jsonFile['author']);
+		$('#google-plus-intent').attr('href','https://plus.google.com/share?text=' + jsonFile['quote']
+			 + ' -' + jsonFile['author']);
 	});
 	
 	$('#generate-btn').on('click', function(){
 			$.getJSON(url, function(jsonFile){
 			$('#sentence').html(jsonFile['quote']);
 			$('#author').html(jsonFile['author']);
+			$('#twitter-intent').attr('href','https://twitter.com/intent/tweet?text=' + jsonFile['quote']
+				 + ' -' + jsonFile['author']);
+			$('#google-plus-intent').attr('href','https://plus.google.com/share?text=' + jsonFile['quote']
+				 + ' -' + jsonFile['author']);
 		});
 	});
-	
+
 });
