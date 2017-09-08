18071a290337f026aa92f997f03d25cccbfce5f9
zeku87 <nocomment182@gmail.com>
2017-09-08 18:54:54 +0200

    color transitions added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 	const url = 'https://talaikis.com/api/quotes/random/';
+	let author = '';
+
 	$.getJSON(url, function(jsonFile){
+		author = jsonFile['author'];
 		$('#sentence').html(jsonFile['quote']);
-		$('#author').html(jsonFile['author']);
+		$('#author-link').html('- ' + jsonFile['author']);
+		$('#author-link').attr('href','https://en.wikipedia.org/wiki/' + jsonFile['author']);
 		$('#twitter-intent').attr('href','https://twitter.com/intent/tweet?text=' + jsonFile['quote']
 		 	 + ' -' + jsonFile['author']);
 		$('#google-plus-intent').attr('href','https://plus.google.com/share?text=' + jsonFile['quote']
@@ -11,13 +15,29 @@ $(document).ready(function(){
 	
 	$('#generate-btn').on('click', function(){
 			$.getJSON(url, function(jsonFile){
-			$('#sentence').html(jsonFile['quote']);
-			$('#author').html(jsonFile['author']);
-			$('#twitter-intent').attr('href','https://twitter.com/intent/tweet?text=' + jsonFile['quote']
-				 + ' -' + jsonFile['author']);
-			$('#google-plus-intent').attr('href','https://plus.google.com/share?text=' + jsonFile['quote']
-				 + ' -' + jsonFile['author']);
-		});
+				author = jsonFile['author'];
+				$('#sentence').html(jsonFile['quote']);
+				$('#author-link').html('- '+jsonFile['author']);
+				$('#author-link').attr('href','https://en.wikipedia.org/wiki/' + jsonFile['author']);
+				$('#twitter-intent').attr('href','https://twitter.com/intent/tweet?text=' + jsonFile['quote']
+					 + ' -' + jsonFile['author']);
+				$('#google-plus-intent').attr('href','https://plus.google.com/share?text=' + jsonFile['quote']
+					 + ' -' + jsonFile['author']);
+			});
+	});
+
+	$('#twitter-icon').hover(function(){
+		$('body').animate({'background-color':'#3399ff'}, 1000);
+	},function(){
+		$('body').animate({'background-color':'black'}, 'slow');
+
+	});
+
+	$('#google-plus-icon').hover(function(){
+		$('body').animate({'background-color':'#ff3333'}, 1000);
+	},function(){
+		$('body').animate({'background-color':'black'}, 'slow');
+
 	});
 
 });
